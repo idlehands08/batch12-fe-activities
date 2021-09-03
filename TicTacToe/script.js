ab4b6e6cbac34ce8438a6b9b0532e0cb006988c7
idlehands08 <86716614+idlehands08@users.noreply.github.com>
2021-09-03 20:04:03 +0800

    added loadHistory function + nextButton/prevButton functions

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,11 +1,13 @@
+//GLOBAL VARIABLES
 const board = document.querySelector('.board');
 const cells = document.querySelectorAll('.cell')
-
 var counter = 0; //used to count the turns
 var lastPlayerTurn = ""; //determines who was the last player to mark move on board. Used to check for winning combinations. 
 const boardArray = []; // initializes our board array
 boardArray[counter] = []; // initializes the first index of our board array as an array as well
 
+//FUNCTIONS
+//IIFE function that initializes board on page load
 (function createBoard() {
     for(let i = 0; i < 3; i++) {
         boardArray[counter][i] = [];
@@ -17,7 +19,6 @@ boardArray[counter] = []; // initializes the first index of our board array as a
         }
     }
     counter += 1;
-    
     console.log(boardArray);
 }())
 
@@ -31,13 +32,13 @@ const playerXTurn = () => {
     }
 }
 
+//handles click event when player clicks on a cell
 const playerMove = (e) => {
     targetCell = e.target;
     if(playerXTurn() === true) {
         targetCell.classList.add('x');
         board.classList.remove('x');
         board.classList.add('o'); //switches turn to o 
-        
     }
     else {
         targetCell.classList.add('o');
@@ -48,15 +49,16 @@ const playerMove = (e) => {
     storeHistory(); //calls the storeHistory function to store current state of board after a player turn
 }
 
+//adds event listeners to our board
 function loadBoardFunctions() {
     const cells = document.querySelectorAll('.cell');
     cells.forEach(cell => {
         cell.addEventListener('click', playerMove, { once: true });
-       
     });
 }
 loadBoardFunctions();
 
+//stores current board state every turn onto our boardArray to be used for previous and next button upon game completion.
 const storeHistory = () => {
     const cells = document.querySelectorAll('.cell'); //get all cells on current turn
     boardArray[counter] = []; // create a new array on boardArray
@@ -94,7 +96,46 @@ const storeHistory = () => {
     boardArray[counter].push(row2);
     boardArray[counter].push(row3);
     counter++; //adds 1 to counter for the next array on boardArray
-   console.log(boardArray); // --for test purposes only--
-    
+    console.log(boardArray); // --for test purposes only--
 }
 
+const previousButton = document.querySelector('.previous');
+
+const previousMove = () => {
+    counter -= 1;
+    loadHistory();
+}
+
+previousButton.addEventListener('click', previousMove);
+
+const nextMove = () => {
+    counter += 1;
+    loadHistory();
+}
+
+const nextButton = document.querySelector('.next');
+
+nextButton.addEventListener('click', nextMove);
+
+const loadHistory = () => {
+    const cells = document.querySelectorAll('.cell'); //get all cells 
+    var cellCounter = 0;
+    boardHistory = boardArray[counter];
+    boardHistory.forEach(row => {
+        row.forEach((item, index) =>{
+            if(item === 'X') {
+                cells[cellCounter].classList.add('x');
+                cellCounter++;
+            }
+            else if(item === 'O') {
+                cells[cellCounter].classList.add('o');
+                cellCounter++;
+            }
+            else {
+                cells[cellCounter].classList.remove('x');
+                cells[cellCounter].classList.remove('o');
+                cellCounter++;
+            }
+        })
+    });
+}
